c25e0f65fc9976994c199b2e155f924eb6e170e3
Easlee Liu <easlee@outlook.com>
2021-12-29 03:34:25 +0000

    upgrade proto -> 3.12.0

--- handler/certificate.go
+++ handler/certificate.go
@@ -13,7 +13,7 @@ import (
 type Certificate struct{}
 
 func (this *Certificate) Get(_ctx context.Context, _req *proto.CerGetRequest, _rsp *proto.CerGetResponse) error {
-	logger.Infof("Received Certificate.Fetch, request is %v", _req)
+	logger.Infof("Received Certificate.Get, request is %v", _req)
 	_rsp.Status = &proto.Status{}
 
 	if "" == _req.Uuid {
@@ -39,6 +39,52 @@ func (this *Certificate) Get(_ctx context.Context, _req *proto.CerGetRequest, _r
 		Consumer:  cer.Consumer,
 		Content:   cer.Content,
 		CreatedAt: cer.CreatedAt.Unix(),
+	}
+
+	return nil
+}
+
+func (this *Certificate) Pull(_ctx context.Context, _req *proto.CerPullRequest, _rsp *proto.CerPullResponse) error {
+	logger.Infof("Received Certificate.Pull, request is %v", _req)
+	_rsp.Status = &proto.Status{}
+
+	if "" == _req.Space {
+		_rsp.Status.Code = 1
+		_rsp.Status.Message = "space is required"
+		return nil
+	}
+
+	if "" == _req.Consumer {
+		_rsp.Status.Code = 1
+		_rsp.Status.Message = "consumer is required"
+		return nil
+	}
+
+	dao := model.NewCertificateDAO(nil)
+
+	cers, err := dao.Query(model.CertificateQuery{
+		Space:    _req.Space,
+		Consumer: _req.Consumer,
+	})
+	if nil != err {
+		_rsp.Status.Code = -1
+		_rsp.Status.Message = err.Error()
+		return nil
+	}
+
+    if nil == cers || 0 == len(cers){
+		_rsp.Status.Code = 2
+		_rsp.Status.Message = "certificate not found"
+		return nil
+    }
+
+	_rsp.Cer = &proto.CertificateEntity{
+		Uuid:      cers[0].UUID,
+		Space:     cers[0].Space,
+		Number:    cers[0].Key,
+		Consumer:  cers[0].Consumer,
+		Content:   cers[0].Content,
+		CreatedAt: cers[0].CreatedAt.Unix(),
 	}
 
 	return nil
